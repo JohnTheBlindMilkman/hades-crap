01ddcce06250d932f84a59dad38e89006cdf7d83
JohnTheBlindMilkman <gosciu0987654321@gmail.com>
2024-02-21 13:31:33 +0100

    First test on lxpool

--- FemtoMerger/femtoMerge.cc
+++ FemtoMerger/femtoMerge.cc
@@ -1,6 +1,8 @@
 #include "indicators.hpp"
 #include "HelperFunctions.hxx"
 #include "TFile.h"
+#include "TH1D.h"
+#include "TH3D.h"
 
 int main(int argc, char **argv)
 {
@@ -25,11 +27,11 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    TString signName(argv[2]);
-    TString bckgName(argv[3]);
-    int ktMax = atoi(argv[4]);
-    int yMax = atoi(argv[5]);
-    int psiMax = atoi(argv[6]);
+    const TString signName(argv[2]);
+    const TString bckgName(argv[3]);
+    const int ktMax = atoi(argv[4]);
+    const int yMax = atoi(argv[5]);
+    const int psiMax = atoi(argv[6]);
 
     if (ktMax < 2 || yMax < 2 || psiMax < 2)
     {
@@ -37,20 +39,96 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    indicators::ProgressBar bar{indicators::option::BarWidth{50},
-                   indicators::option::ForegroundColor{indicators::Color::yellow},
-                   indicators::option::ShowElapsedTime{true},
-                   indicators::option::ShowRemainingTime{true},
-                   indicators::option::PrefixText{"Reading histograms "},
-                   indicators::option::FontStyles{
-                        std::vector<indicators::FontStyle>{indicators::FontStyle::bold}},
-                    indicators::option::MaxProgress{40}};
+    const int maxHistos = ktMax * yMax * psiMax * 2;
+
+    indicators::ProgressBar bar{indicators::option::BarWidth{80},
+                    indicators::option::ForegroundColor{indicators::Color::yellow},
+                    indicators::option::PrefixText{"Reading histograms "},
+                    indicators::option::FontStyles{std::vector<indicators::FontStyle>{indicators::FontStyle::bold}},
+                    indicators::option::MaxProgress{maxHistos}};
 
     indicators::ProgressSpinner spinner{
                     indicators::option::PostfixText{"Merging results"},
                     indicators::option::ForegroundColor{indicators::Color::yellow},
-                    indicators::option::SpinnerStates{
-                        std::vector<std::string>{"⠈", "⠐", "⠠", "⢀", "⡀", "⠄", "⠂", "⠁"}},
-                    indicators::option::FontStyles{
-                        std::vector<indicators::FontStyle>{indicators::FontStyle::bold}}};
+                    indicators::option::SpinnerStates{std::vector<std::string>{"⠈", "⠐", "⠠", "⢀", "⡀", "⠄", "⠂", "⠁"}},
+                    indicators::option::FontStyles{std::vector<indicators::FontStyle>{indicators::FontStyle::bold}}};
+
+    indicators::show_console_cursor(false);
+
+    if (signName.Contains("inv") && bckgName.Contains("inv"))
+    {
+        std::vector<std::vector<std::vector<TH1D*> > > 
+        hSign(ktMax,std::vector<std::vector<TH1D*> >(yMax,std::vector<TH1D*>(psiMax,nullptr))), 
+        hBckg(ktMax,std::vector<std::vector<TH1D*> >(yMax,std::vector<TH1D*>(psiMax,nullptr)));
+
+        auto job1 = [&bar]()
+        {
+            while (true) 
+            {
+                if (bar.is_completed()) 
+                {
+                    bar.set_option(indicators::option::ForegroundColor{indicators::Color::green});
+                    bar.set_option(indicators::option::PrefixText{"✔"});
+                    bar.set_option(indicators::option::HideBarWhenComplete{true});
+                    bar.set_option(indicators::option::ShowPercentage{false});
+                    bar.set_option(indicators::option::PostfixText{"Finished!"});
+                    bar.mark_as_completed();
+                    break;
+                }
+            }
+        };
+
+        std::thread thread1(job1);
+
+        for (int kt = 1; kt <= ktMax; ++kt)
+            for (int y = 1; y <= yMax; ++y)
+                for (int psi = 1; psi <= psiMax; ++psi)
+                {
+                    hSign[kt-1][y-1][psi-1] = inpFile->Get<TH1D>(TString::Format("%s%d%d%d",signName.Data(),kt,y,psi));
+                    if (hSign[kt-1][y-1][psi-1] != nullptr)
+                        hSign[kt-1][y-1][psi-1]->Sumw2();
+                    bar.tick();
+
+                    hBckg[kt-1][y-1][psi-1] = inpFile->Get<TH1D>(TString::Format("%s%d%d%d",bckgName.Data(),kt,y,psi));
+                    if (hBckg[kt-1][y-1][psi-1] != nullptr)
+                        hBckg[kt-1][y-1][psi-1]->Sumw2();
+                    bar.tick();
+                }
+
+        bar.mark_as_completed();
+
+        auto job2 = [&spinner]()
+        {
+            while (true)
+            {
+                if (spinner.is_completed())
+                {
+                    spinner.set_option(indicators::option::PostfixText{"Authenticated!"});
+                    spinner.mark_as_completed();
+                    break;
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(40));
+            }  
+        };
+
+        std::thread thread2(job2);
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+
+        thread1.join();
+        thread2.join();
+    }
+    else if (signName.Contains("osl") && bckgName.Contains("osl"))
+    {
+        std::vector<std::vector<std::vector<TH3D*> > > 
+        hSign(ktMax,std::vector<std::vector<TH3D*> >(yMax,std::vector<TH3D*>(psiMax,nullptr))), 
+        hBckg(ktMax,std::vector<std::vector<TH3D*> >(yMax,std::vector<TH3D*>(psiMax,nullptr)));
+    }
+    else
+    {
+        helper.PrintHelp();
+        indicators::show_console_cursor(true);
+        return 1;
+    }
+    indicators::show_console_cursor(true);
 }
